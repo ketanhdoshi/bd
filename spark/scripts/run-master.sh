2304f8b3880d6a48eea8f914a6feafb909202ac0
Ketan Doshi <fal_thu@yahoo.com>
2020-10-26 20:00:27 +0530

    Got Spark working on laptop

--- spark/scripts/run-master.sh
+++ spark/scripts/run-master.sh
@@ -2,3 +2,4 @@
 
 /usr/local/spark/bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_LOCAL_IP --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
 
+
--- spark/scripts/run-worker.sh
+++ spark/scripts/run-worker.sh
@@ -3,3 +3,4 @@
 /usr/local/spark/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER
 
 
+
